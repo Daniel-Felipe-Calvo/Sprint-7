be4acba5dcdc01ebdf27e2f0a37d8650f440f4cf
Daniel-Felipe-Calvo <calvo_1996@live.com>
2025-01-22 22:03:51 -0500

    Actualización de los graficos

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import pandas as pd
 import plotly_express as px
 
 car_data = pd.read_csv("vehicles_us.csv")
+st.header('Análisis de Datos de Venta de Coches')
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-    # crear un histograma
+    st.write('Gráfico de un histograma para el conjunto de datos de anuncios de venta de coches')
     fig = px.histogram(car_data, x="odometer")
-        
-    # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+    
+scatter_checkbox = st.checkbox('Mostrar gráfico de dispersión')
+if scatter_checkbox:  # al marcar la casilla
+    st.write('Gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig_scatter = px.scatter(car_data, x="odometer", y="price", title="Precio vs. Odómetro")
+    st.plotly_chart(fig_scatter, use_container_width=True)
